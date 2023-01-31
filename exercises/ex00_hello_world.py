f04738c2f4c62e1157c12bf60f384b866b1650ce
Viktoria Ferega <vferega@unc.edu>
2023-01-30 22:56:49 -0500

    Progress on Exercise 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,3 +1,3 @@
-print ("Hello, world. My name is Viktoria.") 
-"""My first program for COMP 110."""
+print("Hello, world. My name is Viktoria.") 
+"""My first program for COMP110."""
 __author__ = "730607734"
---
+++ exercises/lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstartes asking the user for input"""
+
+user_name: str = "Viktoria"
+print("Hello, "+user_name+ ", good morning!")
+print("You are the best programmer ever, "+user_name)
